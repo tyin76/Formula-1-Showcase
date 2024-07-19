6bcb26f16c80e8b564b36e2b01826bfac31dc97b
terenceyin <tyin05@student.ubc.ca>
2024-07-19 00:35:37 -0500

    basically finished drivers page

--- src/components/Driver.js
+++ src/components/Driver.js
@@ -1,137 +1,240 @@
 import React, { useEffect, useState } from 'react'
 import { json } from 'react-router-dom';
 import Typography from '@mui/material/Typography';
-  
+import Max from '../images/Max.png'
+import Lando from '../images/Lando.png'
+import Leclerc from '../images/Leclerc.png'
+import Sainz from '../images/Sainz.png'
+import Piastri from '../images/Piastri.png'
+import Perez from '../images/Perez.png'
+import Russel from '../images/Russel.png'
+import Hamilton from '../images/Hamilton.png'
+import Alonso from '../images/Alonso.png'
+import Stroll from '../images/Stroll.png'
+import Hulk from '../images/Hulk.png'
+import Yuki from '../images/Yuki.png'
+import Ric from '../images/Ric.png'
+import Bearman from '../images/bearman.png'
+import Gasly from '../images/gasly.png'
+import Mag from '../images/Mag.png'
+import Albon from '../images/Albon.png'
+import Ocon from '../images/ocon.png'
+import Zhou from '../images/zhou.png'
+import Sargeant from '../images/sargeant.png'
+import Bottas from '../images/Bottas.png'
 
-function Driver({driverId}) {
+import '../styles/Driver.css'
+import { containerClasses } from '@mui/material';
 
-    const [data, setData] = useState(null)
 
+function Driver({driverId, driverBorder, flag, logo}) {
+
+    const [data, setData] = useState([])
+    var driverIMG;
+    var name;
+    
     switch (driverId) {
         case 4665:
             console.log("MAX")
+            driverIMG = Max
+            name ="Max Verstappen"
             break;
         case 5579: 
             console.log("LANDO")
+            driverIMG = Lando
+            name ="Lando Norris"
             break;
         case 5498:
             console.log("LECLERC")
+            driverIMG = Leclerc
+            name ="Charles Leclerc"
             break;
         case 4686:
             console.log("SAINZ")
+            driverIMG = Sainz
+            name = "Carlos Sainz"
             break;
         case 5752:
             console.log("PIASTRI")
+            driverIMG = Piastri
+            name = "Oscar Piastri"
             break;
         case 4472: 
             console.log("PEREZ")
+            driverIMG = Perez
+            name = "Sergio Perez"
             break;
         case 5503:
             console.log("RUSSEL")
+            driverIMG = Russel
+            name = "George Russel"
             break;
         case 868:
             console.log("HAMILTON")
+            driverIMG = Hamilton
+            name = "Lewis Hamilton"
             break;
         case 348:
             console.log("ALONSO")
+            driverIMG = Alonso
+            name = "Fernando Alonso"
             break;
         case 4775:
             console.log("STROLL")
+            driverIMG = Stroll
+            name = "Lance Stroll"
             break;
         case 4396:
             console.log("HULK")
+            driverIMG = Hulk
+            name = "Nico Hülkenberg"
             break;
         case 5652:
             console.log("YUKI")
+            driverIMG = Yuki
+            name = "Yuki Tsunoda"
             break;
         case 4510:
             console.log("RIC")
+            driverIMG = Ric
+            name = "Daniel Ricciardo"
             break;
         case 5789:
             console.log("Bearman")
+            driverIMG = Bearman
+            name = "Oliver Bearman"
             break;
         case 5501:
             console.log("GASLY")
+            driverIMG = Gasly
+            name = "Pierre Gasly"
             break;
         case 4623:
             console.log("K-MAG")
+            driverIMG = Mag
+            name = "Kevin Magnussen"
             break;
         case 5592:
             console.log("ALBON")
+            driverIMG = Albon
+            name = "Alex Albon"
             break;
         case 4678:
             console.log("OCON")
+            driverIMG = Ocon
+            name = "Esteban Ocon"
             break;
         case 5682:
             console.log("ZHOU")
+            driverIMG = Zhou
+            name = "Zhou Guanyu"
             break;
         case 5745:
             console.log("SARGEANT")
+            driverIMG = Sargeant
+            name = "Logan Sargeant"
             break;
         case 4520:
             console.log("BOTTAS")
+            driverIMG = Bottas
+            name = "Valtteri Bottas"
             break;
     }
 
-    const url = `https://f1-motorsport-data.p.rapidapi.com/stats?driverId=${driverId}`;
-    const options = {
-        method: 'GET',
-        headers: {
-            'x-rapidapi-key': 'a1ac7dbe9bmsh47512af3179ad70p1f25adjsn0d37243aa5dc',
-            'x-rapidapi-host': 'f1-motorsport-data.p.rapidapi.com'
-        }
-};
+//     const url = `https://f1-motorsport-data.p.rapidapi.com/stats?driverId=${driverId}`;
+//     const options = {
+//         method: 'GET',
+//         headers: {
+//             'x-rapidapi-key': 'a1ac7dbe9bmsh47512af3179ad70p1f25adjsn0d37243aa5dc',
+//             'x-rapidapi-host': 'f1-motorsport-data.p.rapidapi.com'
+//         }
+// };
 
         
-useEffect(() => {
-    const getAPI = async () => 
-    {
-        try {
-            const response =  await fetch(url, options);
-            const result =  await response.json();
-            const array = [];
-            for (var i = 0; i < result.length; i++) {
-                const { avgFinish, avgStart, poles, year, starts, top5, top10, rank, wins, points } = result[i];
-                const driver = {
-                    avgFinish: avgFinish,
-                    avgStart: avgStart,
-                    points: points,
-                    poles: poles,
-                    rank: rank,
-                    starts: starts,
-                    top5: top5,
-                    top10: top10,
-                    wins: wins,
-                    year: year
-                  };
-                  array.push(driver)
-            }
+// useEffect(() => {
+//     const getAPI = async () => 
+//     {
+//         try {
+//             const response =  await fetch(url, options);
+//             const result =  await response.json();
+//             const array = [];
+//             for (var i = 0; i < result.length; i++) {
+//                 const { avgFinish, avgStart, poles, year, starts, top5, top10, rank, wins, points } = result[i];
+//                 const driver = {
+//                     avgFinish: avgFinish,
+//                     avgStart: avgStart,
+//                     points: points,
+//                     poles: poles,
+//                     rank: rank,
+//                     starts: starts,
+//                     top5: top5,
+//                     top10: top10,
+//                     wins: wins,
+//                     year: year
+//                   };
+//                   array.push(driver)
+//                   console.log(driver)
+//             }
+            
+//             setData(array)
+//         } catch (error) {
+//             console.error(error);
+//         }
 
-            setData(array)
-        } catch (error) {
-            console.error(error);
-        }
-
-     }
-     getAPI();
-}, [])
-
-var text = "LOADING..."
+//      }
+//      getAPI();
+// }, [])
 
 console.log(data)
-if (data) {
-    text = data[0].year;
-}
 
+    function calculateDriverStats(whichStat) {
+        var stat = 0;
+        if (whichStat === 'year' && data.length > 0) {
+            return data[0][whichStat]
+        }
         
+        if (data.length > 0) {
+            for (var i = 0; i < data.length; i++) {
+            stat = data[i][whichStat] + stat
+
+        }
+    }
+    
+        return stat;
+    }
+
+    function styleDriverCard() {
+        return (
+        <div className={`card-container ${driverBorder}`}>
+
+        <b className='driver-name'>{name}</b>
+        <img src={flag} alt='driver flag' className='driver-flag'></img>
+        
+        <img className='driver-img' src = {driverIMG} alt='Driver Photo'></img>
+
+        <div className="driver-stats">
+        <b>Wins: {calculateDriverStats('wins')} </b>
+        <b>Poles: {calculateDriverStats('poles')} </b>
+        <b>Starts: {calculateDriverStats('starts')} </b>
+        <b>Top 5: {calculateDriverStats('top5')} </b>
+        <b>Top 10: {calculateDriverStats('top10')} </b>
+        <b>First Year in F1: {calculateDriverStats('year')} </b>
+        </div>
+
+
+
+        </div>
+
+        )
+
+
+    }
           
 
         
 return (
    <>
-    <Typography>
-       {text}
-    </Typography>
+    {styleDriverCard()}
    </>
 
     )
--- src/pages/Drivers.js
+++ src/pages/Drivers.js
@@ -1,12 +1,59 @@
 import React from 'react'
 import Driver from '../components/Driver'
+import '../styles/Drivers.css'
+import DutchFlag from '../images/dutch flag.avif'
+import UKFlag from '../images/uk flag.avif'
+import MonacoFlag from '../images/monaco flag.avif'
+import SpainFlag from '../images/spain flag.avif'
+import AusFlag from '../images/aus flag.avif'
+import MexicoFlag from '../images/mexico flag.avif'
+import CanadaFlag from '../images/canada flag.avif'
+import GermanyFlag from '../images/germany flag.avif'
+import JapanFlag from '../images/japan flag.avif'
+import FranceFlag from '../images/france flag.avif'
+import DenmarkFlag from '../images/denmark flag.avif'
+import ThaiFlag from '../images/thai flag.avif'
+import ChinaFlag from '../images/china flag.avif'
+import USAFlag from '../images/usa flag.avif'
+import FinlandFlag from '../images/finland flag.avif'
+import RedBullLogo from '../images/red bull logo.png'
+
 
 function Drivers() {
   return (
-    <div>
-        <Driver driverId={4665}></Driver>
-        {/* <Driver driverId={5579} /> */}
+    <>
+    <div className="driver-page-container">
+      <div className='driver-gallery'>
+        <Driver driverId={4665} driverBorder='red-bull' flag={DutchFlag} logo={RedBullLogo}></Driver>
+        <Driver driverId={5579} driverBorder='mclaren' flag={UKFlag}></Driver>
+        <Driver driverId={5498} driverBorder='ferrari' flag={MonacoFlag}></Driver>
+        <Driver driverId={4686} driverBorder='ferrari' flag={SpainFlag}></Driver>
+        <Driver driverId={5752} driverBorder='mclaren' flag={AusFlag}></Driver>
+        <Driver driverId={4472} driverBorder='red-bull' flag={MexicoFlag}></Driver>
+        <Driver driverId={5503} driverBorder='mercedes' flag={UKFlag}></Driver>
+        <Driver driverId={868} driverBorder='mercedes' flag={UKFlag}></Driver>
+        <Driver driverId={348} driverBorder='aston' flag={SpainFlag}></Driver>
+        <Driver driverId={4775} driverBorder='aston' flag={CanadaFlag}></Driver>
+        <Driver driverId={4396} driverBorder='haas' flag={GermanyFlag}></Driver>
+        <Driver driverId={5652} driverBorder='rb' flag={JapanFlag}></Driver>
+        <Driver driverId={4510} driverBorder='rb' flag={AusFlag}></Driver>
+        <Driver driverId={5501} driverBorder='alpine' flag={FranceFlag}></Driver>
+        <Driver driverId={4623} driverBorder='haas' flag={DenmarkFlag}></Driver>
+        <Driver driverId={5592} driverBorder='williams' flag={ThaiFlag}></Driver>
+        <Driver driverId={4678} driverBorder='alpine' flag={FranceFlag}></Driver>
+        <Driver driverId={5682} driverBorder='kick' flag={ChinaFlag}></Driver>
+        <Driver driverId={5745} driverBorder='williams' flag={USAFlag}></Driver>
+        <Driver driverId={4520} driverBorder='kick' flag={FinlandFlag}></Driver>
+
+
+
+      </div>
     </div>
+    
+    
+    </>
+
+    
   )
 }
 
